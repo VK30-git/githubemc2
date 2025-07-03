f469b97210da35121147e3aadec64b5e98bca9b7
VK30-git <sugumaradvocatessv30@gmail.com>
2025-07-03 14:30:05 +0530

    tailwind completed..project built

---
+++ tailwind/project/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports={
+    content: ["./*.html"],
+    theme:{
+        extend:{
+            screens: {
+                'widescreen' : {'raw': '(min-aspect-ratio: 3/2)'},
+                'tallscreen' : {'raw': '(max-aspect-ratio: 13/20)'},
+            }
+        },
+    },
+    plugins: [],
+}
