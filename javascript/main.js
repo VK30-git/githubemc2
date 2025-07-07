7a76a67fe8faff76be4894a06178264eac6a48dc
VK30-git <sugumaradvocatessv30@gmail.com>
2025-07-07 13:08:59 +0530

    javascript basics and DOM

---
+++ javascript/main.js
@@ -0,0 +1,74 @@
+/* var factor="AK"
+var fplayer="max verstappen"
+var fmovie="f1"
+function favourite()
+{
+    console.log("Favourite Actor:"+factor)
+    console.log("Favourite Player:"+fplayer)
+    console.log("Favourite Movie:"+fmovie)
+}
+favourite() */
+
+/* function add(a,b)
+{
+    console.log(a+b)
+}
+add(10,20) */
+
+/* function area(l,b)
+{
+    console.log("area is "+l*b)
+}
+area(10,20) */
+
+/* function myname()
+{
+    return "vishal"
+}
+var name=myname()
+console.log(name) */
+
+/* function add(a,b)
+{
+    return a+b
+}
+var sum=add(5,5)
+console.log(sum) */
+
+/* if else statement */
+/* var rain=true
+if(rain){
+    console.log("take an umbrella")
+}
+else{
+    console.log("enjoyy")
+} */
+
+/* var color = "red"
+if(color=="red"){
+    console.log("Stop")
+}
+if(color=="oragne")
+{
+    console.log("ready to go")
+}
+if(color=="green"){
+    console.log("GOO!!")
+} */
+
+/* var score=50
+if (score<=50){
+    console.log("you need to improve")
+}
+else if (score>50 && score<=70){
+    console.log("GOod job!")
+}
+else if(score>70){
+    console.log("Excellent performance")
+} */
+
+/* for loop */
+for(i=1;i<=10;i++)
+{
+    console.log(i+"X2="+(i*2))
+}
